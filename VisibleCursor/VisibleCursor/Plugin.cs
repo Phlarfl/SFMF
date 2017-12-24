511028376bc9358fdd0b41e6f06a0095b5402f65
Phlarfl <phlarfl@gmail.com>
2017-12-24 23:45:42 +1300

    Upload VisibleCursor mod
    Signed-off-by: Phlarfl <phlarfl@gmail.com>

---
+++ VisibleCursor/VisibleCursor/Plugin.cs
@@ -0,0 +1,18 @@
+﻿using SFMF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace VisibleCursor
+{
+    public class Plugin : IPlugin
+    {
+        public void Start()
+        {
+            Cursor.visible = true;
+        }
+    }
+}
