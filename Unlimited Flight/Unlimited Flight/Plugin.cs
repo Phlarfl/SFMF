4ca00b6f8dc988320ec1b8904aa7098a35cc0273
Phlarfl <phlarfl@gmail.com>
2017-12-24 19:49:42 +1300

    Upload Unlimited Flight plugin
    Signed-off-by: Phlarfl <phlarfl@gmail.com>

---
+++ Unlimited Flight/Unlimited Flight/Plugin.cs
@@ -0,0 +1,17 @@
+﻿using SFMF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unlimited_Flight
+{
+    public class Plugin : IPlugin
+    {
+        public void Update()
+        {
+            PlayerMovement.Singleton.currentSpeed = Math.Max(800, PlayerMovement.Singleton.currentSpeed);
+        }
+    }
+}
