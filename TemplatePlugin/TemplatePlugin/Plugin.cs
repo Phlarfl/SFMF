eb0e06872f605948bae64e43a8f6b69c73bce02a
Phlarfl <phlarfl@gmail.com>
2017-12-24 19:30:46 +1300

    Upload template plugin
    Signed-off-by: Phlarfl <phlarfl@gmail.com>

---
+++ TemplatePlugin/TemplatePlugin/Plugin.cs
@@ -0,0 +1,17 @@
+﻿using SFMF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemplatePlugin
+{
+    public class Plugin : IPlugin
+    {
+        public void Start()
+        {
+            Console.WriteLine("Template plugin initialized");
+        }
+    }
+}
