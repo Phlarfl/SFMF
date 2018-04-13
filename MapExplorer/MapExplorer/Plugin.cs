61756139f895156979c317b20940f15f7da90c78
Phlarfl <phlarfl@gmail.com>
2018-04-13 19:06:58 +1200

    Add Plugin template
    Signed-off-by: Phlarfl <phlarfl@gmail.com>

---
+++ MapExplorer/MapExplorer/Plugin.cs
@@ -0,0 +1,17 @@
+﻿using SFMF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace MapExplorer
+{
+    public class Plugin : IPlugin
+    {
+        public void Update()
+        {
+        }
+    }
+}
